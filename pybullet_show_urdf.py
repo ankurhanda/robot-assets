80ed757002f9873da4502418413874c3f7a763df
Ankur Handa <ahanda@nvidia.com>
2020-04-12 22:15:31 -0700

    adding bowl and table from https://github.com/code-iai/iai_maps
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- pybullet_show_urdf.py
+++ pybullet_show_urdf.py
@@ -6,7 +6,7 @@ from time import sleep
 
 p.connect(p.GUI)
 
-robot_id = p.loadURDF("/home/ankur/workspace/code/drake/examples/quadrotor/office.urdf", useFixedBase=True)
+robot_id = p.loadURDF("/home/ankur/workspace/code/iai_maps/iai_kitchen/IAI_kitchen.urdf", useFixedBase=True)
 
 p.setGravity(0, -9.81, 0)
 
